145c16b080f4e808610ae00295e4c32c529bd7af
MANIK <2022itb100.manik@students.iiests.ac.in>
2024-07-02 18:22:46 +0530

    auth controllers

--- routes/auth.js
+++ routes/auth.js
@@ -1,51 +1,21 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/user');
 const passport = require('passport');
 const catchAsync = require('../utilities/catchAsync');
-const flash = require('connect-flash');
 const { storeReturnTo } = require('../middleware');
+const auths = require('../controllers/auths')
 
+router.get('/register', auths.registerForm);
 
-router.get('/register', (req, res) => {
-    res.render('auth/register', { title: 'Register' });
-});
+router.post('/register', catchAsync(auths.register));
 
-router.post('/register', catchAsync(async (req, res) => {
-    const { email, username, password } = req.body;
-    try {
-        const user = new User({ email, username });
-        const registeredUser = await User.register(user, password);
-        req.login(registeredUser, err => {
-            if (err) return next(err);
-            req.flash('success', 'Welcome to YelpCamp!');
-            return res.redirect('/camps');
-        });
-    } catch (e) {
-        req.flash('error', e.message);
-        return res.redirect('/register');
-    }
-}));
-
-router.get('/login', (req, res) => {
-    res.render('auth/login', { title: 'Login' });
-});
+router.get('/login', auths.loginForm);
 
 router.post('/login', storeReturnTo, passport.authenticate('local', {
     failureFlash: true,
     failureRedirect: '/login'
-}), (req, res) => {
-    req.flash('success', 'Welcome Back!');
-    const redirectUrl = res.locals.returnTo || '/camps';
-    res.redirect(redirectUrl);
-});
+}), auths.login);
 
-router.get('/logout', (req, res, next) => {
-    req.logout(function (err) {
-        if (err) return next(err);
-        req.flash('success', 'Goodbye!');
-        res.redirect('/camps');
-    });
-});
+router.get('/logout', auths.logout);
 
 module.exports = router;
